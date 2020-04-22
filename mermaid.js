42aeab24911db0a1a48f4289e8dab7e2a7db5389
Ryan Ivan D'Souza <ryan.ivan.dsouza@gmail.com>
2020-04-22 12:14:38 +0530

    Added mermaid wrapper

---
+++ mermaid.js
@@ -0,0 +1,18 @@
+import React from "react";
+import mermaid from "mermaid";
+
+mermaid.initialize({
+  startOnLoad: true,
+  securityLevel: 'loose'
+});
+
+class Mermaid extends React.Component {
+  componentDidMount() {
+    mermaid.contentLoaded();
+  }
+  render() {
+    return <div className="mermaid">{this.props.chart}</div>;
+  }
+}
+
+export default Mermaid;
